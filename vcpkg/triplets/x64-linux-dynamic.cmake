f2ab13e00e49d5787950e1c6abb2381ddd586c38
Mitul Vekariya <vekariya93@gmail.com>
2026-08-09 08:59:26 +0000

    Use x64-linux-dynamic triplet with clang

---
+++ vcpkg/triplets/x64-linux-dynamic.cmake
@@ -0,0 +1,13 @@
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CMAKE_SYSTEM_NAME Linux)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_BUILD_TYPE release)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_FIXUP_ELF_RPATH ON)
+
+# https://learn.microsoft.com/en-us/vcpkg/users/triplets#vcpkg_cmake_configure_options
+# https://learn.microsoft.com/en-us/vcpkg/maintainers/functions/vcpkg_cmake_configure
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS
+    "-DCMAKE_C_COMPILER=clang"
+    "-DCMAKE_CXX_COMPILER=clang++"
+)
